0e70d84757d3aebd880fb324b3f527b76ab85727
Nigel <m0cvo@yahoo.co.uk>
2022-04-04 12:38:47 +0100

    Started work on the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.AddPicture = new System.Windows.Forms.OpenFileDialog();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.PrintBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(796, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AddPicture
+            // 
+            this.AddPicture.FileName = "openFileDialog1";
+            this.AddPicture.FileOk += new System.ComponentModel.CancelEventHandler(this.AddPicture_FileOk);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(677, 384);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 1;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(585, 384);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoadBtn.TabIndex = 2;
+            this.LoadBtn.Text = "Load Picture";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // PrintBtn
+            // 
+            this.PrintBtn.Location = new System.Drawing.Point(490, 384);
+            this.PrintBtn.Name = "PrintBtn";
+            this.PrintBtn.Size = new System.Drawing.Size(75, 23);
+            this.PrintBtn.TabIndex = 3;
+            this.PrintBtn.Text = "Print";
+            this.PrintBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PrintBtn);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private OpenFileDialog AddPicture;
+        private Button ExitBtn;
+        private Button LoadBtn;
+        private Button PrintBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,25 @@ namespace picPrinter
         {
             InitializeComponent();
         }
+
+        private void AddPicture_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            //Using the OpenFileDialog (renamed to AddPicture
+            //The user can upload any picture to the pictureBox.
+            if(AddPicture.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(AddPicture.FileName);
+            }
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
